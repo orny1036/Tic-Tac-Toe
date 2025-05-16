040c48f323303df76687ec9358ef45a6cf40413e
nabila2022831036 <ornynabila@gmail.com>
2025-05-16 21:46:08 +0600

    Added JLabel(textField) to JPanel and JPanel to JFrame.

---
+++ src/ticTacToe.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class ticTacToe implements ActionListener {
+//To determine whose turn it's gonna be first randomly
+    Random random = new Random();
+   JFrame frame = new JFrame();
+   //A panel to hold the title
+   JPanel titlePanel = new JPanel();
+  //A panel to hold al the buttons
+    JPanel buttonPanel = new JPanel();
+//To display messages or some sort
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    ticTacToe()
+    {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textField.setBackground(new Color(0, 0, 0));
+        textField.setForeground(Color.CYAN);
+        textField.setFont(new Font("Ink Free",Font.BOLD,55));
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("Tic-Tac-Toe");
+        textField.setOpaque(true);
+
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0,0,800,100);
+
+        titlePanel.add(textField);
+        frame.add(titlePanel, BorderLayout.NORTH);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    //determine whose turn would be first(random)
+    public void firstTurn()
+    {
+
+    }
+    //checking winning condition
+    public void check()
+    {
+
+    }
+    //situation where X wins(going to receive winning combination of buttons basically)
+    public void xWins(int a, int b)
+    {
+
+    }
+    //situation where Y wins(going to receive winning combination of buttons basically)
+    public void yWins(int a, int b)
+    {
+
+    }
+}
