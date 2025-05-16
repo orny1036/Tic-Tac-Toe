fadec4c6d687e686a623ed357819aabcdf9d0d0f
nabila2022831036 <ornynabila@gmail.com>
2025-05-16 23:02:34 +0600

    initialized the actionPerformed method for all the buttons and set the logic for choosing the first turn randomly and maintaining the turns of X and O

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -42,7 +42,7 @@ public class ticTacToe implements ActionListener {
         {
             buttons[i] = new JButton();
             buttonPanel.add(buttons[i]);
-            buttons[i].setFont(new Font("MV Boli",Font.BOLD,30));
+            buttons[i].setFont(new Font("MV Boli",Font.BOLD,70));
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(this);
 
@@ -56,12 +56,54 @@ public class ticTacToe implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+       for(int i = 0; i<9; ++i)
+       {
+           if(e.getSource() == buttons[i])
+           {
+               if(player1_turn)
+               {
+                   if(buttons[i].getText() == "")
+                   {
+                       buttons[i].setForeground(Color.red);
+                       buttons[i].setText("X");
+                       player1_turn = false;
+                       textField.setText("O turn");
+                   }
+               }
+               else
+               {
+                   if(buttons[i].getText()=="")
+                   {
+                       buttons[i].setForeground(Color.yellow);
+                       buttons[i].setText("O");
+                       player1_turn = true;
+                       textField.setText("X turn");
+                   }
+               }
+           }
+       }
     }
     //determine whose turn would be first(random)
-    public void firstTurn()
-    {
-
+    public void firstTurn() {
+        //To show the title for 2 secs before assigning the player's turn.
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //generate a random number between o and 1. If it's 0 then its the first player's turn and if its 1 then the second player's turn
+        if(random.nextInt(2) == 0)
+        {
+            player1_turn = true;
+            //to show message for X's turn
+            textField.setText("X turn");
+        }
+        else
+        {
+            player1_turn = false;
+            textField.setText("O turn");
+        }
     }
     //checking winning condition
     public void check()
@@ -73,7 +115,7 @@ public class ticTacToe implements ActionListener {
     {
 
     }
-    //situation where Y wins(going to receive winning combination of buttons basically)
+    //situation where O wins(going to receive winning combination of buttons basically)
     public void yWins(int a, int b)
     {
 
